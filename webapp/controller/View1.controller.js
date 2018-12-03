d2a76166af5f25b041c470d1610a6d2804eb6d84
P2000942513 <t.decastro84@gmail.com>
2018-12-03 20:07:23 +0000

    versão 2

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -8,7 +8,7 @@ sap.ui.define([
 	return Controller.extend("desafio-semana-01.desafio-semana-01.controller.View1", {
 		onInit: function () {
 			var data = {
-				isValido: false
+				isValido: true
 			};
 			var oModel = new JSONModel(data);
 			this.getView().setModel(oModel, "layout");
@@ -16,9 +16,9 @@ sap.ui.define([
 		buscaCep: function (oEvent) {
 			var newValue = this.byId("cep").mProperties.value;
 			var Validacep = newValue.replace(/[^0-9]/g, '');
-			var oModelLayout = this.getView().getModel("layout");
-			var oLayoutData = oModelLayout.getData();
-			this.modelLayout = oModelLayout;
+			// var oModelLayout = this.getView().getModel("layout");
+			// var oLayoutData = oModelLayout.getData();
+			// this.modelLayout = oModelLayout;
 			if (Validacep.length == 8) {
 				var oData = `https://viacep.com.br/ws/${newValue}/json/`;
 				var oModel = new JSONModel(oData);
@@ -26,25 +26,25 @@ sap.ui.define([
 				this.getView().setModel(oModel);
 				this.model.attachEvent("requestCompleted", this.onRequestComplete);
 			} else {
-				var oModel = new JSONModel();
-				oLayoutData.isValido = false;
-				oModelLayout.setData(oLayoutData);
-				this.getView().setModel(oModel);
+				// var oModel = new JSONModel();
+				// oLayoutData.isValido = false;
+				// oModelLayout.setData(oLayoutData);
+				// this.getView().setModel(oModel);
 				MessageBox.error("O Cep informado está incompleto!");
 			}
 		},
 		onRequestComplete: function (oEvent) {
-			var oModelLayout = this.modelLayout;
-			var oLayoutData = oModelLayout.getData();
+			// var oModelLayout = this.modelLayout;
+			// var oLayoutData = oModelLayout.getData();
 			if (!oEvent.getParameters().success || this.oData.erro) {
-				var oModel = new JSONModel();
-				this.getView().setModel(oModel); 
-				oLayoutData.isValido = false;
-				oModelLayout.setData(oLayoutData);
+				// var oModel = new JSONModel();
+				// this.getView().setModel(oModel); 
+				// oLayoutData.isValido = false;
+				// oModelLayout.setData(oLayoutData);
 				MessageBox.error("O Cep informado não foi encontrado!");
 			} else {
-				oLayoutData.isValido = true;
-				oModelLayout.setData(oLayoutData); 
+				// oLayoutData.isValido = true;
+				// oModelLayout.setData(oLayoutData); 
 			}
 		}
 
